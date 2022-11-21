e6ddea4248ac940ca214ec67a42b97c4632a6e11
Hebe Lia <heberomeu@gmail.com>
2022-11-21 19:52:20 -0300

    1

--- portfolio-front/src/app/components/navbar/navbar.component.ts
+++ portfolio-front/src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  // crear variables por ej:
+
+  // redes:any;
+  //   visible: boolean =false;
+
+
+  // constructor(private datos: DatosService) { }
+
+// data y redes variables 
+// ngOnInit():void {
+//   this.datos.getDatos().subscribe(data =>{
+//     this.redes = data.redes;
+//   })
+// }
+
+
+// la funcion no retorna nada por el :void
+// make-visible():void{
+//   this.visible=!this.visible;
+//    }
+
+
 }
