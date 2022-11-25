ac2e469b74f11a02ebb16c0afb58cbce7b72f837
Hebe Lia <heberomeu@gmail.com>
2022-11-25 16:15:59 -0300

    json

--- portfolio-front/src/app/components/introduction/introduction.component.ts
+++ portfolio-front/src/app/components/introduction/introduction.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { ServiceService } from 'src/app/services/service.service';
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.css']
 })
-export class IntroductionComponent {
+export class IntroductionComponent implements OnInit{
 
+  name:string='';
+  
+  title:string='';
+  pfp:any;
+  
+  constructor(private data:ServiceService){}
+
+  ngOnInit():void{
+      this.data.getService().subscribe( info =>{
+        this.name =info.name;
+        this.title =info.title;
+        this.pfp =info.pfp;
+      })
+  }
 }
